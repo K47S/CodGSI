9338b50cb7800d4743044bcd40f4a267d50920d0
K47S <michi.volz@gmx.net>
2017-10-02 09:50:51 +0200

    Add project files.

---
+++ Cod2GSI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cod2GSI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
